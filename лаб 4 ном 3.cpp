2db8dfe1db08a1a5d8184f0ed7fefc3c9b8a7f4a
Hummmuss <py0710@yandex.ru>
2021-09-29 22:44:17 +0300

    Update лаб 4 ном 3.cpp

--- лаб 4 ном 3.cpp
+++ лаб 4 ном 3.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 int main()
 {
-	double a, b, c;
-	cin >> a >> b;
-	c = (a + b) / 2;
-	cout << c << endl;
+	double a, b, c, r1, r2, p;
+	cin >> a >> c >> b;
+	if ((a > c) or (c > b))
+	{
+		cout << "error" << endl;
+	}
+	else 
+	{
+		r1 = b - c;
+		r2 = c - a;
+		p = r1 * r2;
+		cout << p << endl;
+	}
 	return 0;
 }
