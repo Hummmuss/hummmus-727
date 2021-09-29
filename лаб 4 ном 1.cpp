0c29196b80a44edeb7ff253b54f1dd471d281fff
Hummmuss <py0710@yandex.ru>
2021-09-30 00:09:49 +0300

    Update лаб 4 ном 1.cpp

--- лаб 4 ном 1.cpp
+++ лаб 4 ном 1.cpp
@@ -1,14 +1,12 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main()
-{
-    double a, b, c, d, r; //вводим вещ. переменные 
-    cout << "vvedite 1 paru koordinat" << endl; //сообщение 
-    cin >> a >> b; //ввод первой пары коорд
-    cout << "vvedite 2 paru koordinat" << endl; //сообщение
-    cin >> c >> d; //ввод второй пары
-    r = sqrt((pow((a - c), 2)) + (pow((b - d), 2))); //вычисления расстояния по формуле
-    cout << r << endl; //вывод результата
+#include <iostream>  // ввод вывод
+using namespace std; // чтобы не писать везде std::
 
+int main() {
+	setlocale(LC_ALL, "Russian"); //библиотека, чтобы русский язык работал
+	double a, b, S, P; //ввод вещественных переменных 
+	cin >> a >> b; // ввод длины и ширины прямоугольника с консоли
+	S = a * b; // считаем площадь
+	P = 2 * (a + b); // считаем периметр
+	cout << "Площадь прямоугольника - " << S << endl; // вывод площади 
+	cout << "Периметр прямоугольника - " << P << endl; //вывод периметра.
 }
