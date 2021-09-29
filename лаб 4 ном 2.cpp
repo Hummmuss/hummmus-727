e01ed36d569365c49274093c86c70f2db49b9208
Hummmuss <py0710@yandex.ru>
2021-09-30 00:09:45 +0300

    Update лаб 4 ном 2.cpp

--- лаб 4 ном 2.cpp
+++ лаб 4 ном 2.cpp
@@ -1,19 +1,10 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main()
-{
-	double a, b, c, r, rr, p;
-	cin >> a >> b >> c;
-	if ((a > b) or (b > c))
-	{
-		cout << "error" << endl;
-	}
-	else
-	{
-		r = c - b;
-		rr = c - a;
-		p = r + rr;
-		cout << p << endl;
-	}
+#include <iostream>   // ввод вывод
+using namespace std; // чтобы не писать везде std::
+
+int main() {
+	double p, d, L; // ввожу вещественные переменные (число пи, диаметр и длину окружности)
+	p = 3.14;
+	cin >> d; //ввод диаметра с консоли
+	L = d * p; // считаем длину окружности
+	cout << "L = " << L << endl; // вывод ответа.
 }
