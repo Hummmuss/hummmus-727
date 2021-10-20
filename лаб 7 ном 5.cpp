53ed41734c9788eb2c4daa4656a52d5b1fd62679
Hummmuss <py0710@yandex.ru>
2021-10-20 23:18:39 +0300

    Create лаб 7 ном 5.cpp

---
+++ лаб 7 ном 5.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float A, B, x;
+    cout << "vvedite A i B" << endl;
+    cin >> A >> B;
+    x = - B / A;
+    cout << x << endl;
+}
