d0a002c794e8dbf18921078298b9543b32953665
Hummmuss <py0710@yandex.ru>
2021-10-20 23:17:41 +0300

    Update лаб 4 ном 3.cpp

--- лаб 4 ном 3.cpp
+++ лаб 4 ном 3.cpp
@@ -4,7 +4,10 @@ using namespace std; // чтобы не писать везде std::
 int main() {
 	setlocale(LC_ALL, "Russian"); //библиотека, чтобы русский язык работал
 	double a, b, C; //вводим вещественные переменные
-	cin >> a >> b; //ввод чисел с консоли
+	cout << "vvedite a" << endl;
+	cin >> a; //ввод чисел с консоли
+	cout << "vvedite b" << endl;
+	cin >> b;
 	C = (a + b) / 2; //считаем ср.ар.
 	cout << "среднее арифметическое - " << C; //вывод
 }
