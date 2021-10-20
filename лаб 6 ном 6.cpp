cf5d5ab977acfde10faf9850610b27635718cb22
Hummmuss <py0710@yandex.ru>
2021-10-20 23:18:15 +0300

    Create лаб 6 ном 6.cpp

---
+++ лаб 6 ном 6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float A, b;
+    cout << "vvedite A" << endl;
+    cin >> A;
+    b = A*A;
+    A = b * b * b;
+    cout << A << endl;
+ 
