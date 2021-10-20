cf3c8f6f5638b3b5662dbb1136ecf26c648e889f
Hummmuss <py0710@yandex.ru>
2021-10-20 23:18:31 +0300

    Create лаб 7 ном 3.cpp

---
+++ лаб 7 ном 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float A, X, Y, C, s;
+    cout << "vvedite posledovatelno X, A, Y" << endl;
+    cin >> X >> A >> Y;
+    C = A / X;
+    s = Y * C;
+    cout << C << endl;
+    cout << s << endl;
+ 
+}
