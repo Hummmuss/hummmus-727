3f9c4dbefeb12a7dc88a00fa2a18840ab1c833ee
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:43:22 +0300

    Update лаб 6 ном 5.cpp

--- лаб 6 ном 5.cpp
+++ лаб 6 ном 5.cpp
@@ -1,3 +1,4 @@
+//Найти значение функции y = 4(x−3)6 − 7(x−3)3 + 2 при данном значении x
 #include <iostream>
 using namespace std;
 #include <cmath>
@@ -6,7 +7,7 @@ int main()
     double x, y, z;
     cout << "vvedite x" << endl;
     cin >> x;
-    z = pow((x - 3), 3);
+    z = pow((x - 3), 3); //вспомогательная переменная (x−3)3
     y = 4 * (pow(z,2)) - 7 * z + 2;
     cout << y << endl;
 }
