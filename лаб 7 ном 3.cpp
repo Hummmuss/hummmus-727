454340fe8bc455adebc95926f06d57815b6ed4da
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:51:31 +0300

    Update лаб 7 ном 3.cpp

--- лаб 7 ном 3.cpp
+++ лаб 7 ном 3.cpp
@@ -1,3 +1,5 @@
+//Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет.
+
 #include <iostream>
 using namespace std;
 int main()
@@ -5,8 +7,8 @@ int main()
     float A, X, Y, C, s;
     cout << "vvedite posledovatelno X, A, Y" << endl;
     cin >> X >> A >> Y;
-    C = A / X;
-    s = Y * C;
+    C = A / X; //сколько стоит 1 кг конфет
+    s = Y * C; //ск стоит У кг конфет
     cout << C << endl;
     cout << s << endl;
  
