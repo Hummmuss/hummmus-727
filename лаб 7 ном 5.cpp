72b363564b2d00ac6b072a6f265939f65be1e1a8
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:53:16 +0300

    Update лаб 7 ном 5.cpp

--- лаб 7 ном 5.cpp
+++ лаб 7 ном 5.cpp
@@ -1,4 +1,4 @@
-
+//Решить линейное уравнение A·x + B = 0, заданное своими коэффициентами A и B (коэффициент A не равен 0).
 #include <iostream>
 using namespace std;
 int main()
@@ -6,6 +6,6 @@ int main()
     float A, B, x;
     cout << "vvedite A i B" << endl;
     cin >> A >> B;
-    x = - B / A;
+    x = - B / A; //так выглядит упрощенное уравнение
     cout << x << endl;
 }
