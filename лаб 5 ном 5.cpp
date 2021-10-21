8b708ca79eeeeaf6e9439b8f5b8d0e4acd8856c8
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:37:37 +0300

    Update лаб 5 ном 5.cpp

--- лаб 5 ном 5.cpp
+++ лаб 5 ном 5.cpp
@@ -8,15 +8,15 @@ int main()
     float x1, y1, x2, y2, x3, y3, a, b, c, S, P;
     cout << "vvedite posledovatelno x1, y1, x2, y2, x3, y3" << endl;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    a = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-    b = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
-    c = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
-    if ((a < (b + c)) and (b < (a + c)) and (c < (a + b)))
+    a = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)); //длина стороны а по формуле
+    b = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2)); //длина стороны b по формуле
+    c = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2)); //длина стороны c по формуле
+    if ((a < (b + c)) and (b < (a + c)) and (c < (a + b))) //проверка на существование треугольника
     {
-        P = (a + b + c) / 2;
-        S = sqrt(P*(P - a)*(P - b)*(P - c));
+        P = (a + b + c) / 2; //полупериметр
+        S = sqrt(P*(P - a)*(P - b)*(P - c)); //площадь по формуле герона
         cout << "S = " << S << endl;
-        cout << "P = " << P << endl;
+        cout << "P = " << P*2 << endl;
     }
     else
     {
