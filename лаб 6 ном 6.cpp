e5e6d559ae95392c3957e10f4eb2d8d52714091d
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:44:05 +0300

    Update лаб 6 ном 6.cpp

--- лаб 6 ном 6.cpp
+++ лаб 6 ном 6.cpp
@@ -1,3 +1,4 @@
+//Дано число A. Вычислить A8 , используя вспомогательную переменную и три операции умножения.
 #include <iostream>
 using namespace std;
 int main()
@@ -5,7 +6,7 @@ int main()
     float A, b;
     cout << "vvedite A" << endl;
     cin >> A;
-    b = A*A;
+    b = A*A; //вспомогательная переменная 
     A = b * b * b;
     cout << A << endl;
  
