a546aea52054f731cf2248455347fb5362494802
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2021-10-21 11:48:20 +0300

    Update лаб 6 ном 7.cpp

--- лаб 6 ном 7.cpp
+++ лаб 6 ном 7.cpp
@@ -1,3 +1,5 @@
+//Дано число A. Вычислить A15, используя две вспомогательные переменные и пять операций умножения.
+
 #include <iostream>
 using namespace std;
 int main()
@@ -5,7 +7,7 @@ int main()
     float A, b;
     cout << "vvedite A" << endl;
     cin >> A;
-    b = pow(A, 2) * A * A * A;
+    b = pow(A, 2) * A * A * A; //вспомогательная переменная
     A = b * b * b;
     cout << A << endl;
 }
